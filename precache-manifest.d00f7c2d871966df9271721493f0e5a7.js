8f466c1f7fc669427cb797e2d133ea4a41bc9078
TrungDT <thanhtrung.it25@gmail.com>
2018-10-25 11:04:41 +0700

    Updates

---
+++ precache-manifest.d00f7c2d871966df9271721493f0e5a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robot-friends-pwa/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "004a52485d648a94279c",
+    "url": "/robot-friends-pwa/static/js/runtime~main.004a5248.js"
+  },
+  {
+    "revision": "e716dfe6179aa8937a78",
+    "url": "/robot-friends-pwa/static/js/main.e716dfe6.chunk.js"
+  },
+  {
+    "revision": "e68a26d54118307cdf4f",
+    "url": "/robot-friends-pwa/static/js/1.e68a26d5.chunk.js"
+  },
+  {
+    "revision": "e716dfe6179aa8937a78",
+    "url": "/robot-friends-pwa/static/css/main.aed2bbd4.chunk.css"
+  },
+  {
+    "revision": "e68a26d54118307cdf4f",
+    "url": "/robot-friends-pwa/static/css/1.7cb081bf.chunk.css"
+  },
+  {
+    "revision": "2b52c055b336708e22c7a0ccff22ffdc",
+    "url": "/robot-friends-pwa/index.html"
+  }
+];
